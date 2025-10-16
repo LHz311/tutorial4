2834ed6362a22aeca508d784c51046762e6889e0
Li Hao zhou <3105168931@qq.com>
2025-10-16 20:06:55 +0800

    Add subtraction test (expected to fail)

---
+++ src/test/java/org/example/CalcTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalcTest {
+    Calc c = new Calc();
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(2, c.subtract(4,2));
+    }
+
+}
